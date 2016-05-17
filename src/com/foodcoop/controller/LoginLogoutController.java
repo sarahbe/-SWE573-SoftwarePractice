9e3731317ac8dc1092154d8f4e6bee8f73a46090
Sarahbeirkdar <sarah.beirkdar@gmail.com>
2016-05-17 20:02:23 +0300

    Roles and login general fix

--- src/com/foodcoop/controller/LoginLogoutController.java
+++ src/com/foodcoop/controller/LoginLogoutController.java
@@ -1,7 +1,6 @@
 package com.foodcoop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,58 +8,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.foodcoop.services.UserService;
- 
-/**
- * Handles and retrieves the login or denied page depending on the URI template
- */
+
 @Controller
 @RequestMapping("/auth")
 public class LoginLogoutController {
-         
- //protected static Logger logger = Logger.getLogger("controller");
- @Autowired
- UserService userService;
- 
- /**
-  * Handles and retrieves the login JSP page
-  * 
-  * @return the name of the JSP page
-  */
- @RequestMapping(value = "/login", method = RequestMethod.GET)
- public String getLoginPage(@RequestParam(value="error", required=false) boolean error, 
-   ModelMap model) {
- // logger.debug("Received request to show login page");
- 
-  // Add an error message to the model if login is unsuccessful
-  // The 'error' parameter is set to true based on the when the authentication has failed. 
-  // We declared this under the authentication-failure-url attribute inside the spring-security.xml
-  /* See below:
-   <form-login 
-    login-page="/krams/auth/login" 
-    authentication-failure-url="/krams/auth/login?error=true" 
-    default-target-url="/krams/main/common"/>*/
-  if (error == true) {
-   // Assign an error message
-   model.put("error", "You have entered an invalid username or password!");
-  } else {
-   model.put("error", "");
-  }
-   
-  // This will resolve to /WEB-INF/jsp/LoginForm.jsp
-  return "LoginForm";
- }
-  
- /**
-  * Handles and retrieves the denied JSP page. This is shown whenever a regular user
-  * tries to access an admin only page.
-  * 
-  * @return the name of the JSP page
-  */
- @RequestMapping(value = "/deniedPage", method = RequestMethod.GET)
-  public String getDeniedPage() {
- // logger.debug("Received request to show denied page");
-   
-  // This will resolve to /WEB-INF/jsp/deniedpage.jsp
-  return "deniedPage";
- }
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(
+			value = "/login",
+			method = RequestMethod.GET)
+	public String getLoginPage(@RequestParam(
+			value = "error",
+			required = false) boolean error, ModelMap model) {
+		if (error == true) {
+			model.put("error",
+					"You have entered an invalid username or password!");
+		} else {
+			model.put("error", "");
+		}
+		return "LoginForm";
+	}
+
+	@RequestMapping(
+			value = "/deniedPage",
+			method = RequestMethod.GET)
+	public String getDeniedPage() {
+
+		return "deniedPage";
+	}
+
+	@RequestMapping(
+			value = "/logoutSuccessful",
+			method = RequestMethod.GET)
+	public String getLogout() {
+
+		return "logoutSuccessful";
+	}
 }
--- src/com/foodcoop/services/CustomUserDetailsService.java
+++ src/com/foodcoop/services/CustomUserDetailsService.java
@@ -3,106 +3,69 @@ package com.foodcoop.services;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
- 
-//import org.apache.log4j.Logger;
-//import com.foodcoop.domain.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
- 
-/**
- * A custom service for retrieving users from a custom datasource, such as a database.
- * <p>
-* This custom service must implement Spring's {@link UserDetailsService}
- */
-@Transactional(readOnly = true)
+
+@Transactional(
+		readOnly = true)
 @Configurable
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-  
-// protected static Logger logger = Logger.getLogger("service");
- 
-// private UserDAO userDAO = new UserDAO();
- @Autowired
- UserService userService;
- 
- 
- /**
-  * Retrieves a user record containing the user's credentials and access. 
-  */
- public UserDetails loadUserByUsername(String username)
-   throws UsernameNotFoundException, DataAccessException {
-   
-  // Declare a null Spring User
-  UserDetails user = null;
-   
-  try {
-    
-   // Search database for a user that matches the specified username
-   // You can provide a custom DAO to access your persistence layer
-   // Or use JDBC to access your database
-   // DbUser is our custom domain user. This is not the same as Spring's User
-   //DbUser dbUser = userDAO.searchDatabase(username);
-	  com.foodcoop.domain.User dbUser = userService.getUserByEmail(username);
-	  
-   // Populate the Spring User object with details from the dbUser
-   // Here we just pass the username, password, and access level
-   // getAuthorities() will translate the access level to the correct role type
- 
-   user = new User(
-     dbUser.getEmail(), 
-     dbUser.getPassword().toLowerCase(),
-     true,
-     true,
-     true,
-     true,
-     getAuthorities(dbUser.getIdRole()) );
- 
-  } catch (Exception e) {
- //  logger.error("Error in retrieving user");
-   throw new UsernameNotFoundException("Error in retrieving user");
-  }
-   
-  // Return user to Spring for processing.
-  // Take note we're not the one evaluating whether this user is authenticated or valid
-  // We just merely retrieve a user that matches the specified username
-  return user;
- }
-  
- /**
-  * Retrieves the correct ROLE type depending on the access level, where access level is an Integer.
-  * Basically, this interprets the access value whether it's for a regular user or admin.
-  * 
-  * @param access an integer value representing the access of the user
-  * @return collection of granted authorities
-  */
-  public Collection<GrantedAuthority> getAuthorities(Integer access) {
-   // Create a list of grants for this user
-   List<GrantedAuthority> authList = new ArrayList<GrantedAuthority>(2);
-    
-   // All users are granted with ROLE_USER access
-   // Therefore this user gets a ROLE_USER by default
-  // logger.debug("Grant ROLE_USER to this user");
-   authList.add(new SimpleGrantedAuthority("ROLE_USER"));
-    
-   // Check if this user has admin access 
-   // We interpret Integer(1) as an admin user
-   if ( access.compareTo(2) == 0) {
-    // User has admin access
-  //  logger.debug("Grant ROLE_ADMIN to this user");
-    authList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-   }
- 
-   // Return list of granted authorities
-   return authList;
-   }
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	UserRoleService userRoleService;
+
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException, DataAccessException {
+
+		UserDetails user = null;
+
+		try {
+			com.foodcoop.domain.User dbUser = userService
+					.getUserByEmail(username);
+			user = new User(dbUser.getEmail(),
+					dbUser.getPassword().toLowerCase(), true, true, true, true,
+					getAuthorities(dbUser));
+
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("Error in retrieving user");
+		}
+
+		return user;
+	}
+
+	public Collection<GrantedAuthority> getAuthorities(
+			com.foodcoop.domain.User user) {
+		// Create a list of grants for this user
+		List<GrantedAuthority> authList = new ArrayList<GrantedAuthority>(2);
+
+		if (user.getMemberActive()) {
+			authList.add(new SimpleGrantedAuthority("ROLE_USER"));
+		}
+		if (user.getAdminActive()) {
+			authList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+		if (user.getSalerActive()) {
+			authList.add(new SimpleGrantedAuthority("ROLE_SALER"));
+		}
+		if (user.getStockerActive()) {
+			authList.add(new SimpleGrantedAuthority("ROLE_STOCKER"));
+		}
+
+		// Return list of granted authorities
+		return authList;
+	}
 }
