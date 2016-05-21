32424a785e677e89cfdce996d5a88a939a0e8a90
sarahbe <sarah.beirkdar@gmail.com>
2016-05-21 13:44:54 +0300

    registeration bug

--- src/com/foodcoop/controller/HomePageController.java
+++ src/com/foodcoop/controller/HomePageController.java
@@ -18,103 +18,108 @@ import com.foodcoop.services.UserService;
 @Controller
 public class HomePageController {
 
- @Autowired
- UserService userService;
- 
- @Autowired 
- UserRoleService userRoleService;
+	@Autowired
+	UserService userService;
 
- //The @RequestMapping annotation on the methods use the value attribute to map the method to a path.
- //The beginning of the path is given by context:component-scan
- @RequestMapping("/register")
- public ModelAndView registerUser(@ModelAttribute User user) {
+	@Autowired
+	UserRoleService userRoleService;
 
-  List<String> genderList = new ArrayList<String>();
-  genderList.add("male");
-  genderList.add("female");
+	// The @RequestMapping annotation on the methods use the value attribute to
+	// map the method to a path.
+	// The beginning of the path is given by context:component-scan
+	@RequestMapping("/register")
+	public ModelAndView registerUser(@ModelAttribute User user) {
+		List<String> genderList = new ArrayList<String>();
+		genderList.add("male");
+		genderList.add("female");
 
-  Map<String, List> map = new HashMap<String, List>();
-  map.put("genderList", genderList);
-  return new ModelAndView("register", "map", map);
- }
- 
- @RequestMapping("/")
- public ModelAndView index() {
+		Map<String, List> map = new HashMap<String, List>();
+		map.put("genderList", genderList);
+		return new ModelAndView("register", "map", map);
+	}
 
-  
-  return new ModelAndView("index");
- }
- 
- 
- @RequestMapping( "/insert")
- public String inserData(@ModelAttribute User user) {
-  if (user != null){
-   userService.insertData(user);
+	@RequestMapping("/")
+	public ModelAndView index() {
 
-  }
-  return "redirect:/";
- }
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping("/SuccessfullRegister")
+	public ModelAndView SuccessfullRegister() {
 
- @RequestMapping("/getList")
- public ModelAndView getUserLIst() {
-  List<User> userList = userService.getUserList();
-  return new ModelAndView("userList", "userList", userList);
- }
+		return new ModelAndView("SuccessfullRegister");
+	}
+	
+	@RequestMapping("/insert")
+	public String inserData(@ModelAttribute User user) {
+			if (user != null) {
+				userService.insertData(user);
+				return "redirect:/SuccessfullRegister";
+			}
+		
+	
+		return "redirect:/register";
+	}
 
- @RequestMapping("/edit")
- public ModelAndView editUser(@RequestParam String id,
-   @ModelAttribute User user) {
+	@RequestMapping("/getList")
+	public ModelAndView getUserLIst() {
+		List<User> userList = userService.getUserList();
+		return new ModelAndView("userList", "userList", userList);
+	}
 
-  user = userService.getUser(id);
+	@RequestMapping("/edit")
+	public ModelAndView editUser(@RequestParam String id, @ModelAttribute User user) {
 
-  List<String> genderList = new ArrayList<String>();
-  genderList.add("male");
-  genderList.add("female");
+		user = userService.getUser(id);
 
-  Map<String, Object> map = new HashMap<String, Object>();
-  map.put("genderList", genderList);
-  map.put("user", user);
+		List<String> genderList = new ArrayList<String>();
+		genderList.add("male");
+		genderList.add("female");
 
-  return new ModelAndView("edit", "map", map);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("genderList", genderList);
+		map.put("user", user);
 
- }
+		return new ModelAndView("edit", "map", map);
 
- @RequestMapping("/update")
- public String updateUser(@ModelAttribute User user) {
-  userService.updateData(user);
-  return "redirect:/getList";
+	}
 
- }
+	@RequestMapping("/update")
+	public String updateUser(@ModelAttribute User user) {
+		userService.updateData(user);
+		return "redirect:/getList";
 
- @RequestMapping("/delete")
- public String deleteUser(@RequestParam String id) {
-  System.out.println("id = " + id);
-  userService.deleteData(id);
-  return "redirect:/getList";
- }
- 
- @RequestMapping("/approve")
- public String approveUser(@RequestParam String id,@RequestParam boolean active) {	 
-  userService.grantUserRole(id,1, active);
-  return "redirect:/getList";
- }
- 
- @RequestMapping("/seller")
- public String setSeller(@RequestParam String id,@RequestParam boolean active) {
- userService.grantUserRole(id, 3,active);
-  return "redirect:/getList";
- }
- 
- @RequestMapping("/stocker")
- public String setStocker(@RequestParam String id,@RequestParam boolean active) {
-	 userService.grantUserRole(id, 4, active);
-  return "redirect:/getList";
- }
- 
- @RequestMapping("/admin")
- public String setAdmin(@RequestParam String id,@RequestParam boolean active) {
-	 userService.grantUserRole(id, 2,active);
-  return "redirect:/getList";
- }
- 
+	}
+
+	@RequestMapping("/delete")
+	public String deleteUser(@RequestParam String id) {
+		System.out.println("id = " + id);
+		userService.deleteData(id);
+		return "redirect:/getList";
+	}
+
+	@RequestMapping("/approve")
+	public String approveUser(@RequestParam String id, @RequestParam boolean active) {
+		userService.grantUserRole(id, 1, active);
+		return "redirect:/getList";
+	}
+
+	@RequestMapping("/seller")
+	public String setSeller(@RequestParam String id, @RequestParam boolean active) {
+		userService.grantUserRole(id, 3, active);
+		return "redirect:/getList";
+	}
+
+	@RequestMapping("/stocker")
+	public String setStocker(@RequestParam String id, @RequestParam boolean active) {
+		userService.grantUserRole(id, 4, active);
+		return "redirect:/getList";
+	}
+
+	@RequestMapping("/admin")
+	public String setAdmin(@RequestParam String id, @RequestParam boolean active) {
+		userService.grantUserRole(id, 2, active);
+		return "redirect:/getList";
+	}
+
 }
